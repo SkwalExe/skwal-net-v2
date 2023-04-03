3932cdd36f0afc4de64167191e8957128e298b64
SkwalExe <support@skwal.net>
2023-04-03 21:23:25 +0200

    fix error in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,14 @@
 # application back online.
 
 mv maintenance/maintenance.html maintenance/ACTIVE/maintenance.html
+sleep 5
 git reset --hard HEAD
 git clean -f -d
 git pull origin main
-sleep 5
-mv maintenance/ACTIVE/maintenance.html maintenance/maintenance.html
+
+# The git reset and git clean commands will automatically 
+# move the maintenance.html file back to the maintenance 
+# directory. So we dont need to do that.
+# mv maintenance/ACTIVE/maintenance.html maintenance/maintenance.html
 
 echo Done!
