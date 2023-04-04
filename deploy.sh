19be32d9942dfe2c8bdc5ac03da1238bfd3b3f0e
SkwalExe <support@skwal.net>
2023-04-04 16:54:01 +0200

    prevent the deploy script from running in a local environment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@
 # from the git repository, and then bring the
 # application back online.
 
+if [ $HOSTNAME != "esx1" ]
+then
+  echo This script must only be run in production! 
+  exit 1 
+fi
+
 touch maintenance/ACTIVE
 echo Enabling maintenance mode
 sleep 5
