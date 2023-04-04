c6ae74503f49179d176852bc50f07a635fbce38e
Léopold Koprivnik <support@skwal.net>
2023-04-04 16:42:01 +0200

    use a file outside the repository to enable maintenance mode (#14)

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 # from the git repository, and then bring the
 # application back online.
 
-mv maintenance/maintenance.html maintenance/ACTIVE/maintenance.html
+touch maintenance/ACTIVE
+echo Enabling maintenance mode
 sleep 5
+
 git reset --hard HEAD
 git clean -f -d
 git pull origin main
 
-# The git reset and git clean commands will automatically 
-# move the maintenance.html file back to the maintenance 
-# directory. So we dont need to do that.
-# mv maintenance/ACTIVE/maintenance.html maintenance/maintenance.html
-
+rm -f maintenance/ACTIVE
+echo Disabling maintenance mode
+sleep 5
 echo Done!
