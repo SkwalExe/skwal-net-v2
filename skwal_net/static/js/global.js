c80eaba3f432c14171ce82e0e0a2fe6d354d10d1
Léopold Koprivnik <support@skwal.net>
2023-04-11 17:20:44 +0200

    add footer (#33)

--- skwal_net/static/js/global.js
+++ skwal_net/static/js/global.js
@@ -1,2 +1,6 @@
 toasteur = new Toasteur("top-right", 5000, 'top');
 Toultip.init(200, true);
+
+// Call resize event to force the navbar and 
+// other elements to adjust to the window size
+window.dispatchEvent(new Event("resize"));
