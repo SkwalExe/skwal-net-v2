81bff01a28849ebd551af2696a7e071f2f94c001
SkwalExe <support@skwal.net>
2023-04-15 15:48:51 +0200

    fix error in paths

--- skwal_net/documents/views.py
+++ skwal_net/documents/views.py
@@ -2,24 +2,27 @@ from django.shortcuts import render
 import markdown
 import os
 from django.http import HttpResponse
+from django.conf import settings
 
 
 def index(request):
     module = request.GET.get("module", "introduction")
     section = request.GET.get("section", "introduction") + ".md"
 
-    modules = os.listdir("documents/sections")
+    modules = os.listdir(os.path.join(
+        settings.BASE_DIR, "documents", "sections"))
     if module not in modules:
         return HttpResponse("Module not found", status=404)
 
-    sections = os.listdir(f"documents/sections/{module}")
+    sections = os.listdir(os.path.join(
+        settings.BASE_DIR, "documents", "sections", module))
     if section not in sections:
         return HttpResponse("Section not found", status=404)
 
     section_content = open(
-        f"documents/sections/{module}/{section}", "r").read()
+        os.path.join(settings.BASE_DIR, "documents", "sections", module, section), "r").read()
     sidebar_content = open(
-        f"documents/sections/{module}/_sidebar.md", "r").read()
+        os.path.join(settings.BASE_DIR, "documents", "sections", module, "_sidebar.md"), "r").read()
 
     return render(request, "docs/index.html", {
         "section_content": section_content,
