d4ceeef6a198d6b63accbc6ea2b6a6fa4c84261c
SkwalExe <support@skwal.net>
2023-04-19 15:15:22 +0200

    add my certificates

--- skwal_net/static/js/links.js
+++ skwal_net/static/js/links.js
@@ -1,15 +1,26 @@
 $$("*[href]").forEach(element => {
     let href = element.getAttribute("href");
-    element.onclick = (e) =>
+    element._onclick = (e) =>
         redirect(href, (e.ctrlKey || e.metaKey || e.button == 1))
-    element.onauxclick = (e) => {
-        if (e.button === 1)
+    element._onauxclick = (e) => {
+        if (e.button === 2)
             redirect(href, true);
     }
 });
 
 $$("*[_href]").forEach(element => {
     let href = element.getAttribute("_href");
-    element.onclick = element.onauxclick = () =>
-        redirect(href, true);
+    element.onmousedown = (e) => {
+        if (e.button !== 2)
+            redirect(href, true);
+    }
+});
+
+$$("*[_href], *[href]").forEach(element => {
+    let href = element.getAttribute("_href");
+    element.oncontextmenu = () => {
+        copy(href);
+        toasteur.success("URL copied to clipboard", "Copied!");
+        return false;
+    }
 });
