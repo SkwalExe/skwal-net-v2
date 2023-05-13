e080c12dd92e9140f1dcb3eb68896756df9ad0c7
SkwalExe <support@skwal.net>
2023-05-13 22:28:47 +0200

    update nginx config to adapt to new server env

---
+++ nginx_update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script will update the nginx
+# configuration and reload nginx.
+if [ $HOSTNAME != "skwal-server" ]
+then
+  echo This script must only be run in production!
+  exit 1
+fi
+
+echo Dont forget to run as root
+
+rm -f /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+cat configs/nginx/sites-available/skwal_net > /etc/nginx/sites-available/skwal_net
+ln -s /etc/nginx/sites-available/skwal_net /etc/nginx/sites-enabled/skwal_net
