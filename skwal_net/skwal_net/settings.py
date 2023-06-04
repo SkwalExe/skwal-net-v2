836762806705bd9839dd5b4977dca7a1d7fc1fd7
skwal <SkwalExe>
2023-06-04 18:43:36 +0200

    email utilities, new function send_mail and smtp config in .env

--- skwal_net/skwal_net/settings.py
+++ skwal_net/skwal_net/settings.py
@@ -205,3 +205,12 @@ LOGIN_URL = "/login"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+# Email settings
+EMAIL_HOST=os.getenv("EMAIL_HOST")
+EMAIL_PORT=os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS=os.getenv("EMAIL_USE_TLS") == "true"
+EMAIL_USE_SSL=os.getenv("EMAIL_USE_SSL") == "true"
+DEFAULT_FROM_EMAIL=os.getenv("DEFAULT_FROM_EMAIL")
