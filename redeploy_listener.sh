6c9ed5275a9727e3e7781db2338a21d790ab43b6
skwal <SkwalExe>
2023-06-04 09:42:36 +0200

    prevent errors in auto redeploy system

--- redeploy_listener.sh
+++ redeploy_listener.sh
@@ -3,8 +3,11 @@
 # and if it exists, it will pull the latest changes
 # from the git repository by calling deploy.sh.
 
+cd /var/www/skwal-net-v2/
+
 while true; do
   if [ -f hooks/redeploy ]; then
+    echo Redeploying...
     rm -f hooks/redeploy
     ./deploy.sh
   fi
