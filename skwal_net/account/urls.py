bcfa20de0100652c9ff0995de7838f9222c05c0b
Léopold Koprivnik <skwal.net@gmail.com>
2023-06-06 12:43:22 +0200

    change profile page path, add useful links and base for the settings page (#93)
    Co-authored-by: skwal <SkwalExe>

--- skwal_net/account/urls.py
+++ skwal_net/account/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("register/", views.register_, name="register"),
     path("login/", views.login_, name="login"),
     path("profile/", views.profile_, name="profile"),
-    path("profile/<str:username>/", views.profile_, name="profile"),
+    path("profile/@<str:username>/", views.profile_, name="profile"),
     path("logout/", views.logout_, name="logout"),
+    path("settings/", views.settings_, name="settings"),
 ]
--- skwal_net/main/classes.py
+++ skwal_net/main/classes.py
@@ -1,3 +1,10 @@
+from django.urls import reverse_lazy as reverse
+
+# Important, NavButtons are not Navbar Buttons.
+# They are only used for the navbar right now, 
+# But they arent made only for this.
+# They Can be used to links etc...
+
 class NavButton():
     def __init__(self, href, text, icon):
         self.href = href
@@ -7,27 +14,40 @@ class NavButton():
 
 class HomeNavButton(NavButton):
     def __init__(self):
-        super().__init__("/", "Home", "fa fa-home")
+        super().__init__(reverse("home"), "Home", "fa fa-home")
 
 
 class AccountNavButton(NavButton):
     def __init__(self):
-        super().__init__("/profile", "Account", "fa fa-user")
+        super().__init__(reverse("profile"), "Account", "fa fa-user")
 
 
 class LoginNavButton(NavButton):
     def __init__(self):
-        super().__init__("/login", "Login", "fa fa-sign-in-alt")
+        super().__init__(reverse("login"), "Login", "fa fa-sign-in-alt")
+
 
 class OnionSiteNavButton(NavButton):
     def __init__(self):
         super().__init__("http://h452fxjm2mv44hqytxahde4kjgg5tlbgd2un4yiqcfk3s35c2w63uryd.onion", "Tor Onion Site", "fa fa-globe")
 
+
 class BlogNavButton(NavButton):
     def __init__(self):
         super().__init__("https://blog.skwal.net", "My Blog (fr)", "fa fa-book")
 
-# I will need to add more types like info, success, warning...
+        
+class LogoutNavButton(NavButton):
+    def __init__(self):
+        super().__init__(reverse("logout"), "Logout", "fa fa-sign-out-alt")
+
+
+class SettingsNavButton(NavButton):
+    def __init__(self):
+        super().__init__(reverse("settings"), "Settings", "fa fa-cog")
+
+
+        # I will need to add more types like info, success, warning...
 allowed_header_types = [
     "", 
     "error", 
